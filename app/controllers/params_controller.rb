4c9639e318cdc02d0b0b5f34007c7bebb7fb0427
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-16 19:43:18 -0500

    add number guess route

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,5 +1,11 @@
 class ParamsController < ApplicationController
   def show_name
+    name = "matthias"
+    p name
+    render json: name.upcase.as_json
+  end
+
+  def high_low
     render json: { message: "hello" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/params_game", controller: "params", action: "show_name"
+  get "/number_guess", controller: "params", action: "high_low"
 end
